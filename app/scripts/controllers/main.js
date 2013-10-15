b909488e725055505dae9fdbe63697dede84f7f7
Are Almaas <mralmaas@gmail.com>
2013-10-15 09:53:30 +0200

    scaffold application

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('volleyballviewerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
