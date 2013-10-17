88c106bd20f5354259d91b48ff37c261641cde75
ksundmyhr <kristoffer.sundmyhr@mwnooslmaclt124.local>
2013-10-17 13:36:56 +0200

    Update angular version, add animate effects, remove iframe

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -8,28 +8,29 @@ app.controller('DetailCtrl', ['$scope', '$route', 'PDFViewerService', function (
 	$scope.id = $route.current.params.id;
 
     // $scope.pdfURL = "assets/pdfviewer/test.pdf";
-	$scope.pdfURL = "https://s3-eu-west-1.amazonaws.com/volleyballpdf/pdf/Magnes+volleyballkr_nike++1978-1979+-+bind+1_OCR.pdf";
-    $scope.instance = pdf.Instance("viewer");
+    // $scope.pdfURL = "https://s3-eu-west-1.amazonaws.com/volleyballpdf/pdf/Magnes+volleyballkr_nike++1978-1979+-+bind+1_OCR.pdf";
+    // $scope.instance = pdf.Instance("viewer");
 
-    $scope.nextPage = function() {
-        $scope.instance.nextPage();
-    };
 
-    $scope.prevPage = function() {
-        $scope.instance.prevPage();
-    };
+    // $scope.nextPage = function() {
+    //     $scope.instance.nextPage();
+    // };
 
-    $scope.gotoPage = function(page) {
-        $scope.instance.gotoPage(page);
-    };
+    // $scope.prevPage = function() {
+    //     $scope.instance.prevPage();
+    // };
 
-    $scope.pageLoaded = function(curPage, totalPages) {
-        $scope.currentPage = curPage;
-        $scope.totalPages = totalPages;
-    };
+    // $scope.gotoPage = function(page) {
+    //     $scope.instance.gotoPage(page);
+    // };
 
-    $scope.loadProgress = function(loaded, total, state) {
-        console.log('loaded =', loaded, 'total =', total, 'state =', state);
-    };
+    // $scope.pageLoaded = function(curPage, totalPages) {
+    //     $scope.currentPage = curPage;
+    //     $scope.totalPages = totalPages;
+    // };
+
+    // $scope.loadProgress = function(loaded, total, state) {
+    //     console.log('loaded =', loaded, 'total =', total, 'state =', state);
+    // };
 
 }]);
